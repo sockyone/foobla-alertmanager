bdfc9ac4c6a66a92d5607d24c12995b8238dce03
NamPhan <sockyone16899@gmail.com>
2020-05-20 10:36:26 +0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 
 const PORT = 3991
 
+app.use(require('body-parser').json())
+app.use(require('cors')())
+
 app.post('/alert', (req, res)=>{
     console.log(req.body)
     res.send("OK")
